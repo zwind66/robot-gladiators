ec6617c8d97bfed8d8a543d41dfda7f94ff3dc11
Shuai Zuo <szuo1@vols.utk.edu>
2022-04-26 17:07:11 -0500

    using while Loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,7 +10,7 @@ var enemyAttack = 12;
 
 var fight = function (enemyName) {
   // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+   while(enemyHealth > 0) {
 
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -65,6 +65,9 @@ var fight = function (enemyName) {
     window.alert("You need to choose a valid option. Try again!");
   }
 }
-for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+}
+for (var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
