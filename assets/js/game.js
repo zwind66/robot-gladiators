45afd51b216febd31dc63e84cf38c77995c5c2b6
Shuai Zuo <szuo1@vols.utk.edu>
2022-04-28 18:21:56 -0500

    add bestscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -160,11 +160,27 @@ var startGame = function() {
 var endGame = function() {
   window.alert("The game has now ended. Let's see how you did!");
 
+  // check localStorage for high score, if it's not there, use 0
+  var highScore = localStorage.getItem("highscore");
+  highScore = highScore || 0;
+
   // if player is still alive, player wins!
   if (playerInfo.health > 0) {
     window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + '.');
-  } else {
+  } 
+  else {
     window.alert("You've lost your robot in battle!");
+  }
+
+   // if player has more money than the high score, player has new high score!
+   if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  } 
+  else {
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
   }
 
   // ask player if they'd like to play again
@@ -172,10 +188,11 @@ var endGame = function() {
 
   if (playAgainConfirm) {
     startGame();
-  } else {
+  } 
+  else {
     window.alert('Thank you for playing Robot Gladiators! Come back soon!');
   }
-};1
+};
 
 // go to shop between battles function
 var shop = function() {
